6ce9cc39307f4970bb5f35897c50fd2603ccf601
Sunpil Kim <ezweb28@gmail.com>
2022-08-18 20:46:12 -0400

    CH 22. Create Service Layer

---
+++ src/main/java/guru/springframework/netflux/Services/MovieServiceImpl.java
@@ -0,0 +1,25 @@
+package guru.springframework.netflux.Services;
+
+import guru.springframework.netflux.Repositories.MovieRepository;
+import guru.springframework.netflux.domain.Movie;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
+    @Override
+    public Mono<Movie> getMovieById(String id) {
+        return movieRepository.findById(id);
+    }
+
+    @Override
+    public Flux<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+}
